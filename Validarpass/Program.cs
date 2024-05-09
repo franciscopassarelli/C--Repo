e22078959714daa87fd9ab21fb59af6ff1f8d710
francisco passarelli <118229728+franciscopassarelli@users.noreply.github.com>
2024-05-09 01:49:03 -0300

    proyecto
    validar pass

---
+++ Validarpass/Program.cs
@@ -0,0 +1,30 @@
+﻿int intentos = 0;
+
+string contrasenia;
+
+bool ingreso = true;
+
+Console.WriteLine("ingrese su contraseña");
+
+while (ingreso)
+{
+    contrasenia = Console.ReadLine();
+    intentos++;
+    if (contrasenia.Equals("usSystem-2022"))
+    {
+        ingreso = false;
+        Console.WriteLine("Bienvenidos a coderhouse dev");
+        break;
+    }
+
+  else if (intentos >= 5) 
+    {
+        Console.WriteLine("su contraseña es incorrecta y se agotaron los intentos");
+        break;
+
+    }
+
+    Console.WriteLine("su contraseña fue invalida, intente nuevamente");
+   
+
+}
